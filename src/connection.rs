f4655030b33955c6122af684107fce394a9455c7
philipp <philipp3923@icloud.com>
2023-04-21 21:25:07 +0200

    connect

---
+++ src/connection.rs
@@ -0,0 +1,73 @@
+use chrono::Duration;
+use std::io::{empty, Empty, Error};
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+use std::num::ParseIntError;
+use std::time;
+use async_io::Timer;
+use chrono::{Timelike, Utc};
+use socket2::{Domain, SockAddr, Socket, Type};
+
+pub struct Connection {
+    socket_address: SocketAddr,
+    socket: Socket,
+    connected: bool
+}
+
+impl Connection {
+
+    pub(crate) fn new(ipv6: &str, port: u16) -> Result<Connection, String>{
+        let mut parts_str = ipv6.split(":").into_iter();
+        let mut parts: [u16; 8] = [0; 8];
+
+
+        for i in 0..8 as usize {
+            let next_str = match parts_str.next() {
+                None => return Err("ipv6 address is to short".to_string()),
+                Some(num) => num
+            };
+
+            parts[i] = match u16::from_str_radix(next_str, 16) {
+                Ok(num) => num,
+                Err(_) => return Err("Invalid ipv6 address".to_string())
+            };
+        }
+
+        let ipv6_addr = Ipv6Addr::from(parts);
+        let socket_address = SocketAddr::new(IpAddr::from(ipv6_addr), port);
+        let socket = Socket::new(Domain::IPV6, Type::STREAM, None).unwrap();
+
+        let bind_ipv6 = SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0)), 0);
+        socket.bind(&SockAddr::from(bind_ipv6)).unwrap();
+
+        return Ok( Connection {
+            socket_address,
+            socket,
+            connected: false
+        });
+    }
+
+    pub fn get_port(&self) -> u16{
+        return self.socket.local_addr().unwrap().as_socket().unwrap().port();
+    }
+
+    pub async fn connect(&mut self, attempts: u8) -> Result<(),String> {
+        let now = Utc::now();
+
+        for _ in 0..attempts {
+            let target = now.with_nanosecond(0).unwrap() + Duration::seconds(1);
+            let diff = target - now;
+
+            Timer::after(time::Duration::from_nanos(diff.num_nanoseconds().unwrap() as u64)).await;
+
+            if self.socket.connect(&SockAddr::from( self.socket_address)).is_ok() {
+                self.connected = true;
+                return Ok(());
+            }
+        }
+
+
+        return Err("was not able to connect".to_string());
+    }
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+extern crate core;
+
+use std::error::Error;
+
+mod connection;
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut connection = connection::Connection::new("2001:7c7:2159:2f00:79da:1d32:cee6:f77",2000).unwrap();
+
+    connection.connect(10).await;
+
+    return Ok(());
 }
