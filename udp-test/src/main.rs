a396d3ab0bb3c6ff4b7b9e655223d45bdf497caf
philipp <philipp3923@icloud.com>
2023-04-28 21:36:23 +0200

    ipv6

--- udp-test/src/main.rs
+++ udp-test/src/main.rs
@@ -1,23 +1,26 @@
-use std::net::{IpAddr, SocketAddr, UdpSocket};
+use std::net::{IpAddr, Ipv6Addr, SocketAddr, UdpSocket};
 use std::str::FromStr;
 use std::time::Duration;
 use connection::time::Synchronizer;
 
 fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:2001").expect("binding socket failed");
-    let address = IpAddr::from_str("91.56.29.75").expect("failed to parse ip");
+    let bind = IpAddr::from(Ipv6Addr::new(0,0,0,0,0,0,0,0));
+    let socket = UdpSocket::bind(SocketAddr::new(bind, 2000)).expect("binding socket failed");
+    let address = IpAddr::from_str("2A02:3031:20B:B72C:6A6E:EEF7:D49D:A1A2").expect("failed to parse ip");
     let socket_address = SocketAddr::new(address, 2000);
     let mut synchro = Synchronizer::new(false).expect("failed to create synchronizer");
     let mut count: u8 = 0;
 
-    socket.set_read_timeout(Some(Duration::from_millis(100))).expect("failed to change timeout");
+    socket.set_read_timeout(Some(Duration::from_millis(10))).expect("failed to change timeout");
     socket.set_nonblocking(false).expect("failed to set blocking");
 
     loop {
         std::thread::sleep(synchro.wait_time());
 
         socket.send_to([count].as_slice(), socket_address).expect("failed to send");
+
         count+=1;
+        println!("Sent: {}",count);
         let mut buf = [0u8; 1];
 
         match socket.recv_from(buf.as_mut_slice()) {
