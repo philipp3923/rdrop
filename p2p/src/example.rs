5b2ca376a652a64f2fffe98b22cd21e4e94fefef
philipp <philipp3923@icloud.com>
2023-05-08 22:25:53 +0200

    example.rs

---
+++ p2p/src/example.rs
@@ -0,0 +1,29 @@
+use std::net::Ipv6Addr;
+use std::str::FromStr;
+use std::time::Duration;
+use crate::client::{ClientReader, ClientWriter};
+use crate::protocol::Connection;
+
+#[allow(dead_code)]
+fn main() {
+
+    let timeout = Duration::from_secs(60);
+
+    let ipv6 = Ipv6Addr::from_str("ENTER IPV6 ADDRESS HERE").unwrap();
+
+    let connection = Connection::new(Some(2000)).unwrap();
+
+    let connection = connection.connect(ipv6, 2000, Some(timeout), Some(timeout)).unwrap();
+
+    let connection = connection.encrypt().unwrap();
+
+    let connection = connection.upgrade().unwrap();
+
+    let (mut writer, mut reader) = connection.accept();
+
+    writer.write(b"Das ist ein Test").unwrap();
+
+    let response = reader.read(Some(timeout)).unwrap();
+
+    println!("{}", String::from_utf8(response).unwrap());
+}
--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod client;
 pub mod protocol;
+mod example;
