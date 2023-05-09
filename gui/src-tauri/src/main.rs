ea56c42aa99f63b8c98b8dce4bbad7957dc943ca
philipp <philipp3923@icloud.com>
2023-05-09 21:53:36 +0200

    event update

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -3,10 +3,12 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
+use tauri::{AppHandle, Manager, Wry};
 use window_shadows::set_shadow;
 use std::time::Duration;
 use std::thread::sleep;
+use p2p::error::Error;
+use p2p::protocol::{Connection, Waiting};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -15,14 +17,22 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+
     tauri::Builder::default()
         .setup(|app| {
             let handle = app.handle();
-            let event_handler = EventHandler::new(handle.clone());
             let window = app.get_window("main").unwrap();
-            set_shadow(&window, true).expect("Unsupported platform!");
+            match set_shadow(&window, true) {
+                Ok(_) => {println!("WINDOWS")}
+                Err(_) => {println!("LINUX")}
+            };
 
-            app.listen_global("app://add-file", |event| {
+            match Connection::new(None) {
+                Ok(connection) => run(handle, connection),
+                Err(err) => run_err(handle, err)
+            }
+
+            /*app.listen_global("app://add-file", |event| {
                 println!("got app://add-file with payload {:?}", event.payload());
             });
 
@@ -32,9 +42,7 @@ fn main() {
                 event_handler.send_update_status("Punshing holes", "Real deep", false);
                 sleep(Duration::new(2, 0));
                 event_handler.send_update_status("Failed to establish connection", "Timeout", true);
-            });
-
-
+            });*/
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
@@ -42,18 +50,38 @@ fn main() {
         .expect("error while running tauri application");
 }
 
+fn run_err(handle: AppHandle<Wry>, err: p2p::error::Error) {
+    let event_handler = EventHandler::new(handle.clone());
+}
+
+fn run(handle: AppHandle<Wry>, connection: Connection<Waiting>) {
+    let event_handler = EventHandler::new(handle.clone());
+}
+
 struct EventHandler {
-    handle: tauri::AppHandle,
+    handle: AppHandle<Wry>,
 }
 
 impl EventHandler {
-    fn new(handle: tauri::AppHandle) -> Self {
+    fn new(handle: AppHandle<Wry>) -> Self {
         Self { handle }
     }
 
-    fn send_update_status(&self, status: &str, description: &str, error: bool) {
+    fn send_connect_status(&self, status: &str, description: &str) {
         self.handle
-            .emit_all("app://update-status", Status { status: status.into(), description: description.into(), error: error})
+            .emit_all("app://update-status", Status { status: status.into(), description: description.into(), error: false})
+            .unwrap();
+    }
+
+    fn send_connect_error(&self, status: &str, description: &str) {
+        self.handle
+            .emit_all("app://update-status", Status { status: status.into(), description: description.into(), error: false})
+            .unwrap();
+    }
+
+    fn send_init_error(&self, status: &str, description: &str) {
+        self.handle
+            .emit_all("app://update-status", Status { status: status.into(), description: description.into(), error: false})
             .unwrap();
     }
 }
