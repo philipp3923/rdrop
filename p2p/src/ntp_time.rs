c7b0e5e86c73591184bdc2885b938557432a43cb
philipp <philipp3923@icloud.com>
2023-05-12 00:16:52 +0200

    sntp domain -> ip

--- p2p/src/ntp_time.rs
+++ p2p/src/ntp_time.rs
@@ -29,15 +29,15 @@ struct UdpSocketWrapper(UdpSocket);
 impl NtpUdpSocket for UdpSocketWrapper {
     fn send_to<T: ToSocketAddrs>(&self, buf: &[u8], addr: T) -> sntpc::Result<usize> {
         match self.0.send_to(buf, addr) {
-            Ok(size) => Ok(size),
-            Err(_err) => Err(sntpc::Error::Network)
+            Ok(usize) => Ok(usize),
+            Err(_) => Err(sntpc::Error::Network),
         }
     }
 
     fn recv_from(&self, buf: &mut [u8]) -> sntpc::Result<(usize, SocketAddr)> {
         match self.0.recv_from(buf) {
-            Ok(res) => Ok(res),
-            Err(_err) => Err(sntpc::Error::Network)
+            Ok((size, addr)) => Ok((size, addr)),
+            Err(_) => Err(sntpc::Error::Network),
         }
     }
 }
@@ -46,9 +46,21 @@ pub fn get_diff() -> Result<(Duration, i64), crate::error::Error> {
     let ntp_context = NtpContext::new(StdTimestampGen::default());
     let socket = UdpSocketWrapper(UdpSocket::bind("0.0.0.0:0").expect("something"));
     //#TODO change time server to be dynamic
-    let result = sntpc::get_time("time.cloudflare.com:123", socket, ntp_context)?;
+    let result = sntpc::get_time("83.168.200.199:123", socket, ntp_context);
 
     println!("{:?}", result);
 
+    let result = result?;
+
     Ok((Duration::from_micros(result.offset as u64), result.offset))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_time() {
+        assert!(get_diff().is_ok());
+    }
 }
