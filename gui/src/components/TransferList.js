8e765010f2aef3cd69ab2532cfa312838bdfcc20
Lars Zocher <lars@ikozo.de>
2023-05-13 14:21:58 +0200

    Fix bug

--- gui/src/components/TransferList.js
+++ gui/src/components/TransferList.js
@@ -3,7 +3,7 @@ import MatIcon from './MatIcon';
 import IconButton from './IconButton';
 import useTauriEvent from './hooks/useTauriEvent';
 import { emit } from '@tauri-apps/api/event';
-import {filesize} from 'filesize';
+import { filesize } from 'filesize';
 import { invoke } from '@tauri-apps/api/tauri';
 import { FileState } from '../vendor/file';
 import { save } from '@tauri-apps/api/dialog';
@@ -12,25 +12,24 @@ export default function TransferList() {
     const [hover, setHover] = useState(false);
     const [files, setFiles] = useState([]);
 
-    useEffect(() => {
-    }, []);
+    useEffect(() => {}, []);
 
     const mutateFile = (file) => {
         if (typeof file?.size !== 'string') {
-            file.size = filesize(file.size, { standard: "jedec"});
+            file.size = filesize(file.size, { standard: 'jedec' });
         }
         return file;
-    }
+    };
 
     useTauriEvent('tauri://file-drop', (event) => {
         console.log(event);
 
-        if(event.payload.length > 0) {
+        if (event.payload.length > 0) {
             event.payload.forEach((file) => {
                 invoke('offer_file', { path: file });
             });
         }
-        
+
         setHover(false);
     });
 
@@ -44,18 +43,16 @@ export default function TransferList() {
 
     useTauriEvent('app://file-update', (event) => {
         const file = mutateFile(event.payload);
-        let newFiles = files.map((f) => f.hash === file.hash ? file : f);
-        if(!newFiles.some((f) => f.hash === file.hash))
-            newFiles = [file, ...newFiles];
-        setFiles(newFiles);
+        setFiles((files) => {
+            let newFiles = files.map((f) => (f.hash === file.hash ? file : f));
+            if (!newFiles.some((f) => f.hash === file.hash)) newFiles = [file, ...newFiles];
+            return newFiles;
+        });
     });
 
-
     const handleCancel = (file) => {
-        if(file.state === FileState.PENDING)
-            invoke('deny_file', { hash: file.hash });
-        else if(file.state === FileState.TRANSFERING)
-            invoke('stop_file', { hash: file.hash });
+        if (file.state === FileState.PENDING) invoke('deny_file', { hash: file.hash });
+        else if (file.state === FileState.TRANSFERING) invoke('stop_file', { hash: file.hash });
     };
 
     const handleDownload = async (file) => {
@@ -83,7 +80,7 @@ export default function TransferList() {
                     const canCancel = file.state !== 'completed' && !file.is_sender;
                     const canShowInExplorer = file.state === 'completed' && !file.is_sender;
                     return (
-                        <div className={'transfer-list-item' + (file.is_sender ? " sender" : "")} key={file.id}>
+                        <div className={'transfer-list-item' + (file.is_sender ? ' sender' : '')} key={file.id}>
                             <div className='transfer-list-item-icon'>
                                 {file.is_sender && <MatIcon>vertical_align_top</MatIcon>}
                                 {!file.is_sender && <MatIcon>vertical_align_bottom</MatIcon>}
@@ -93,14 +90,26 @@ export default function TransferList() {
                                 <p className='body-large'>{file.size}</p>
                             </div>
                             <div className='transfer-list-item-status'>
-                                {file.state === "pending" && <p className='body-large'>Pending</p>}
-                                {file.state === "transfering" && <p className='body-large'>{file.status}%</p>}
-                                {file.state === "completed" && <p className='body-large'>Completed</p>}
+                                {file.state === 'pending' && <p className='body-large'>Pending</p>}
+                                {file.state === 'transfering' && <p className='body-large'>{file.status}%</p>}
+                                {file.state === 'completed' && <p className='body-large'>Completed</p>}
                             </div>
                             <div className='transfer-list-item-actions flex'>
-                                {canDownload && <IconButton text onClick={()=>handleDownload(file)}>download</IconButton>}
-                                {canCancel && <IconButton text onClick={()=>handleCancel(file)}>close</IconButton>}
-                                {canShowInExplorer && <IconButton text onClick={()=>handleShowInExplorer(file)}>folder</IconButton>}
+                                {canDownload && (
+                                    <IconButton text onClick={() => handleDownload(file)}>
+                                        download
+                                    </IconButton>
+                                )}
+                                {canCancel && (
+                                    <IconButton text onClick={() => handleCancel(file)}>
+                                        close
+                                    </IconButton>
+                                )}
+                                {canShowInExplorer && (
+                                    <IconButton text onClick={() => handleShowInExplorer(file)}>
+                                        folder
+                                    </IconButton>
+                                )}
                             </div>
                         </div>
                     );
