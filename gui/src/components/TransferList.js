70e1d1e8eec7a4deebeed5bd52253c9bff8b6517
Lars Zocher <lars@ikozo.de>
2023-05-14 15:39:44 +0200

    Remove dummy file entries

--- gui/src/components/TransferList.js
+++ gui/src/components/TransferList.js
@@ -39,7 +39,7 @@ export default function TransferList() {
             path: 'C:\\Users\\User\\Downloads\\test.txt',
         }];
 
-        setFiles(files);
+        // setFiles(files);
     }, []);
 
     useEffect(() => {
