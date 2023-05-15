3af26df6a39557ede8e5287e6264c640305b5697
philipp <philipp3923@icloud.com>
2023-05-15 04:22:14 +0200

    idk

--- p2p/src/client/udp.rs
+++ p2p/src/client/udp.rs
@@ -540,7 +540,7 @@ impl ClientHandler {
                     header
                 }
                 None => {
-                    if self.message_receive_buffer.len() < SLIDE_WINDOW as usize / 10 {
+                    if self.message_receive_buffer.len() == 0 {
                         sleep(RECEIVE_INTERVAL);
                     }
                     continue;
@@ -661,7 +661,7 @@ impl ClientHandler {
         self.message_send_buffer.iter().for_each(|package| {
             i += 1;
             if package.timestamp.elapsed() > SEND_INTERVAL {
-                sleep(Duration::from_nanos(10));
+                sleep(Duration::from_nanos(1));
                 if let Err(e) = self.udp_socket.send(package.content.as_slice()) {
                     println!("[UDP] send error: {:?}", e);
                 }
@@ -685,7 +685,7 @@ impl ClientHandler {
                 Ok(content) => {
                     let (content, size) = ClientHandler::encode_msg(&content, MessageType::Data, self.send_counter);
                     //println!("SEND number: {} size: {} content {:2x?}", self.send_counter, size, content);
-                    sleep(Duration::from_nanos(10));
+                    sleep(Duration::from_nanos(1));
                     self.udp_socket.send(content.as_slice())?;
                     self.message_send_buffer.push(Package::new(content, size, self.send_counter, MessageType::Data));
                     self.send_counter = self.send_counter.wrapping_add(1);
